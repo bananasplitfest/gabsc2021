4bb1dec639e45cb348a95830664c65c8a33c756f
Isaac McDaniel <isaac@Isaacs-MacBook-Pro.local>
2022-07-29 19:15:14 -0500

    Updated activity locations, activity schedule. Added address to schedule page. Change sorting and colors on calendar page

--- src/static/js/fullcalendar.js
+++ src/static/js/fullcalendar.js
@@ -1,7 +1,7 @@
 import activitiesJson from '../data/2022-schedule.json' assert {type: 'json'};
 const activities = activitiesJson.activities;
 for (const a of activities){
-    if (a.resourceId == "" || !a.resourceId) a.resourceId = "invalid"
+    if (a.resourceId == "" || !a.resourceId) a.resourceId = "invalid";
 }
 import resourcesJson from '../data/resources.json' assert {type: 'json'};
 const resources = resourcesJson.resources;
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: "resourceTimeline",
     resources: resources,
-    resourceOrder: 'title',
+    resourceOrder: 'sortOrder',
     schedulerLicenseKey: "CC-Attribution-NonCommercial-NoDerivatives",
     events: activities,
     slotMinTime: '7:00:00',
