e3e83084430935abbea2223547e81625bd2c91ca
Isaac McDaniel <isaac@Isaacs-MacBook-Pro.local>
2022-08-02 00:41:44 -0500

    Updated schedule data file. Added entertainer social links and sponsors to car show

--- src/static/js/fullcalendar.js
+++ src/static/js/fullcalendar.js
@@ -1,22 +1,36 @@
-import activitiesJson from '../data/2022-schedule.json' assert {type: 'json'};
-const activities = activitiesJson.activities;
-for (const a of activities){
-    if (a.resourceId == "" || !a.resourceId) a.resourceId = "invalid";
-}
-import resourcesJson from '../data/resources.json' assert {type: 'json'};
-const resources = resourcesJson.resources;
-
-console.log(activities);
-console.log(resources);
-
 document.addEventListener("DOMContentLoaded", function () {
   var calendarEl = document.getElementById("calendar");
   var calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: "resourceTimeline",
-    resources: resources,
+    resources: async function fetchActivities() {
+      let response = await fetch('../data/resources.json');
+    
+      console.log(response.status); // 200
+      console.log(response.statusText); // OK
+    
+      if (response.status === 200) {
+        let data = await response.json();
+        const resources = data.resources;
+        return resources;
+      }
+    },
     resourceOrder: 'sortOrder',
     schedulerLicenseKey: "CC-Attribution-NonCommercial-NoDerivatives",
-    events: activities,
+    events: async function fetchActivities() {
+      let response = await fetch('../data/2022-schedule.json');
+    
+      console.log(response.status); // 200
+      console.log(response.statusText); // OK
+    
+      if (response.status === 200) {
+        let data = await response.json();
+        const activities = data.activities;
+        for (const a of activities){
+            if (a.resourceId == "" || !a.resourceId) a.resourceId = "invalid";
+        }
+        return activities;
+      }
+    },
     slotMinTime: '7:00:00',
     slotMaxTime: '22:00:00',
     now: '2022-08-19',
@@ -36,5 +50,5 @@ document.addEventListener("DOMContentLoaded", function () {
       },
 
   });
-  calendar.render();
+  calendar.render();  
 });
